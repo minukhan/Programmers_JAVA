b87487a83b09ddc77ed2968a217d6e96db089d0c
MINWOOK HAN <dnrals7929@naver.com>
2024-06-10 23:39:22 +0900

    deletechar

---
+++ deletechar.java
@@ -0,0 +1,25 @@
+class deletechar {
+    public String deletechar(String my_string, String letter) {
+
+        
+        char[] test = new char[my_string.length()];
+        char[] test2 = new char[my_string.length()];
+        char mess = letter.charAt(0);
+        int k =0;
+        
+        
+        test = my_string.toCharArray();
+        
+        for(int i=0;i<my_string.length();i++){
+            if(test[i]!=mess){
+                
+                test2[k] = test[i];
+                k++;
+            }
+        }
+        
+        String answer = new String(test2 ,0, k);
+        
+        return answer;
+    }
+}
