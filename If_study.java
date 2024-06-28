0d6e3eed8ce4e0c753201cdc9d47a153111315ba
HANMINUOOK <dnrals7929@naver.com>
2024-06-28 16:56:41 +0900

    If_study

---
+++ If_study.java
@@ -0,0 +1,10 @@
+class If_study {
+    public int If_study(int num, int n) {
+        int answer = 0;
+        
+        if(num%n==0){
+            answer=1;
+        }
+        return answer;
+    }
+}
