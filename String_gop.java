0c23dd05c1ec2b20f20e58ff166a7e1ba363053b
MINWOOK HAN <dnrals7929@naver.com>
2024-06-29 08:31:25 +0900

    String_gop

---
+++ String_gop.java
@@ -0,0 +1,10 @@
+class String_gop {
+    public String String_gop(String my_string, int k) {
+        String answer = "";
+        
+        for(int i=0;i<k;i++){
+            answer += my_string;
+        }
+        return answer;
+    }
+}
