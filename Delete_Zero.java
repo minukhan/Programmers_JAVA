c137eecbc15dae50a3a0a61f6567aa5b64f13c58
HANMINUOOK <dnrals7929@naver.com>
2024-06-30 19:43:34 +0900

    Delete_Zero

---
+++ Delete_Zero.java
@@ -0,0 +1,11 @@
+class Delete_Zero {
+    public String Delete_Zero(String n_str) {
+        String answer = "";
+        int number =0;
+        
+        number = Integer.parseInt(n_str);
+        answer = number+"";
+        
+        return answer;
+    }
+}
