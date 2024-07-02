8be715e3fae790c8c9b5805bb5157c683915c85c
HANMINUOOK <dnrals7929@naver.com>
2024-07-03 03:47:32 +0900

    rny_string

---
+++ rny_string.java
@@ -0,0 +1,10 @@
+class rny_string {
+    public String rny_string(String rny_string) {
+        String answer = "";
+        
+        answer = rny_string.replace("m","rn");
+        
+        
+        return answer;
+    }
+}
