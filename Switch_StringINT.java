227f9be4379243a5bc26f7caf8437a3e82c0480b
HANMINUOOK <dnrals7929@naver.com>
2024-07-03 02:15:03 +0900

    Switch_StringINT

---
+++ Switch_StringINT.java
@@ -0,0 +1,9 @@
+class Switch_StringINT {
+    public int Switch_StringINT(String n_str) {
+        int answer = 0;
+        
+        answer = Integer.parseInt(n_str);
+        
+        return answer;
+    }
+}
