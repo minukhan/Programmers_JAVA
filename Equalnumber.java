e1402b39cb2d3195fe677ea4e2458c8f1280c15d
HANMINUOOK <dnrals7929@naver.com>
2024-07-04 17:06:56 +0900

    Equalnumber

---
+++ Equalnumber.java
@@ -0,0 +1,11 @@
+class Equalnumber {
+    public int Equalnumber(int number, int n, int m) {
+        int answer = 0;
+        
+        if(number%n==0 && number%m==0){
+            answer =1;
+        }
+        
+        return answer;
+    }
+}
