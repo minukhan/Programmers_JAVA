bc2ba88b3ec73de37573b685b2aaac2f592eec55
HANMINUOOK <dnrals7929@naver.com>
2024-07-09 14:27:40 +0900

    Change_Order

---
+++ Change_Order.java
@@ -0,0 +1,16 @@
+class Change_Order {
+    public int[] Change_Order(int[] num_list, int n) {
+        int[] answer = new int[num_list.length] ;
+        int count=0;
+        
+        for(int i=n;i<num_list.length;i++){
+            answer[count++]= num_list[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            answer[count++]= num_list[i];
+        }
+        
+        return answer;
+    }
+}
