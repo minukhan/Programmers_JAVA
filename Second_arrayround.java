7ae3625fe85b9623ba2783011287a36d28ca9335
HANMINUOOK <dnrals7929@naver.com>
2024-07-09 20:12:41 +0900

    Second_arrayround

---
+++ Second_arrayround.java
@@ -0,0 +1,15 @@
+class Second_arrayround {
+    public int Second_arrayround(int[][] board, int k) {
+        int answer = 0;
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(i+j<=k){
+                    answer += board[i][j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
