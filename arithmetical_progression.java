62017b7989066af4966c7f856ec786a72f189f48
HANMINUOOK <dnrals7929@naver.com>
2024-07-09 21:52:38 +0900

    arithmetical_progression

---
+++ arithmetical_progression.java
@@ -0,0 +1,13 @@
+class arithmetical_progression {
+    public int arithmetical_progression(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        for(int i=0;i<included.length;i++){
+            
+            if(included[i]){
+                answer += a+d*(i);
+            }
+        }
+        return answer;
+    }
+}
