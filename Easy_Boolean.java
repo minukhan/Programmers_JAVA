ebdd9284101bf834f76bdc66352ca92dfa0c6bf9
HANMINUOOK <dnrals7929@naver.com>
2024-07-11 09:33:02 +0900

    Easy_Boolean

---
+++ Easy_Boolean.java
@@ -0,0 +1,11 @@
+class Easy_Boolean {
+    public boolean Easy_Boolean(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = false;
+        
+        if((x1 || x2) && (x3 || x4)){
+            answer = true;
+        }
+        
+        return answer;
+    }
+}
