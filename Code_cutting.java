eeacb3fa59485c65a99562e55c50ac63cffa40d1
HANMINUOOK <dnrals7929@naver.com>
2024-07-14 17:16:04 +0900

    Code_cutting

---
+++ Code_cutting.java
@@ -0,0 +1,33 @@
+class Code_cutting {
+    public String Code_cutting(String code) {
+        String answer = "";
+        int mode=0;
+        
+        for(int i=0;i<code.length();i++){
+            if(mode==0){
+                if(code.charAt(i)!='1' && i%2==0){
+                    answer+=code.charAt(i);
+                }
+                
+                if(code.charAt(i)=='1'){
+                    mode =1;
+                }
+            }
+            else{
+                if(code.charAt(i)!='1' && i%2==1){
+                    answer+=code.charAt(i);
+                }
+                
+                if(code.charAt(i)=='1'){
+                    mode =0;
+                }
+            }
+        }
+        
+        if(answer.equals("")){
+            answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+}
