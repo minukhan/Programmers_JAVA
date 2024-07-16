f834b4a3c7e974244f127149fc3bf6715f960dc3
HANMINUOOK <dnrals7929@naver.com>
2024-07-16 14:00:01 +0900

    Triangle_document

---
+++ Triangle_document.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Triangle_document {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+
+        System.out.println(b_square);
+
+    }
+
+}
