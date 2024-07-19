b43c4e1ab2bd57bc89f7af493eab970a787afdb2
HANMINUOOK <dnrals7929@naver.com>
2024-07-19 11:24:36 +0900

    Convert_String_INTEGER

---
+++ Convert_String_INTEGER.java
@@ -0,0 +1,9 @@
+class Convert_String_INTEGER {
+    public int Convert_String_INTEGER(String s) {
+        int answer = 0;
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
