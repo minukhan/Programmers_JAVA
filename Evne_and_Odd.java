3e81bad200e830a5de042b8ee2f3b37944d64f31
HANMINUOOK <dnrals7929@naver.com>
2024-07-22 16:19:03 +0900

    Evne_and_Odd

---
+++ Evne_and_Odd.java
@@ -0,0 +1,14 @@
+class Evne_and_Odd {
+    public String Evne_and_Odd(int num) {
+        String answer = "";
+        
+        if(num%2==0){
+            answer = "Even";
+        }
+        else{
+            answer = "Odd";
+        }
+        
+        return answer;
+    }
+}
