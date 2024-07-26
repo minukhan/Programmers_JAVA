6ebc26e6f3646d0d5a5fc9b3d17c6e4db2565926
HANMINUOOK <dnrals7929@naver.com>
2024-07-26 20:16:01 +0900

    Eatting_Championship

---
+++ Eatting_Championship.java
@@ -0,0 +1,23 @@
+class Eatting_Championship {
+    public String Eatting_Championship(int[] food) {
+        String answer = "";
+        
+        for(int i=1;i<food.length;i++){
+            for(int j=0;j<food[i]/2;j++){
+                answer+= i;
+            }
+        }
+        
+        answer += '0';
+        
+        for(int i=food.length-1;i>0;i--){
+            for(int j=0;j<food[i]/2;j++){
+                answer+= i;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
