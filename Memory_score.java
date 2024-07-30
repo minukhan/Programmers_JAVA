c305b2c1a43491872a35c111a2cf585030e912e9
HANMINUOOK <dnrals7929@naver.com>
2024-07-30 12:26:40 +0900

    Memory_score

---
+++ Memory_score.java
@@ -0,0 +1,24 @@
+class Memory_score {
+
+    public int[] Memory_score(String[] name, int[] yearning, String[][] photo) {
+
+        int[] answer = new int[photo.length];
+        for(int i=0;i<photo.length;i++){
+
+            for(int j=0;j<photo[i].length;j++){
+
+                for(int k=0;k<name.length;k++){
+
+                    if(photo[i][j].equals(name[k])){
+
+                        answer[i] += yearning[k];
+                    }
+                }
+            }
+        }
+
+        return answer;
+
+    }
+
+}
