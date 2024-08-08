98cf3d39fca17eb610e587cb4c0e5fcdbc52c3d2
MINWOOK HAN <dnrals7929@naver.com>
2024-08-08 23:38:11 +0900

    Soldier_weapon

---
+++ Soldier_weapon.java
@@ -0,0 +1,28 @@
+class Soldier_weapon {
+    public int Soldier_weapon(int number, int limit, int power) {
+        int answer = 0;
+        int count =0;
+        
+        for(int i=1;i<=number;i++){
+            count =0;
+
+            for(int j=1;j*j<=i;j++){
+                if(i%j==0){
+                    count++;
+                    if(j!=i/j){
+                        count++;
+                    }
+                }
+            }
+            
+            if(count>limit){
+                answer+= power;
+            }
+            else{
+                answer += count;
+            }
+        }
+        
+        return answer;
+    }
+}
