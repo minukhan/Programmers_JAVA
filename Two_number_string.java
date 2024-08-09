9e83ed25ed3c4a9269e80dac3a3dd8b33c70fa89
HANMINUOOK <dnrals7929@naver.com>
2024-08-09 23:37:55 +0900

    modifiy

--- Two_number_string.java
+++ Two_number_string.java
@@ -17,7 +17,7 @@ class Two_number_string {
         }
         
         for(int i=0;i<len;i++){
-            
+              
             if(alen){
                 if(i>=b.length()){
                     anum = a.charAt(a.length()-i-1)-'0';
