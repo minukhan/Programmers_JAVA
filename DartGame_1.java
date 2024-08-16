2510dc2cbd60727d8345e06b8005da672d3e0cac
HANMINUOOK <dnrals7929@naver.com>
2024-08-16 12:39:00 +0900

    DartGame_1

---
+++ DartGame_1.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        int[] numberarr = new int[3];
+        String num = "";
+        char dart;
+        int score=0;
+        int index =0;
+        boolean next = false;
+        
+        for(int i=0;i<dartResult.length();i++){
+            dart = dartResult.charAt(i);
+            if(Character.isDigit(dart)){
+                num+=dart;
+            }
+            else if(dart == 'S'){
+                score = Integer.parseInt(num);
+                numberarr[index] = score;
+                next=true;
+                num= "";
+            }
+            else if(dart == 'D'){
+                score = Integer.parseInt(num);
+                numberarr[index] = score * score;
+                next=true;
+                num= "";
+            }
+            else if(dart == 'T'){
+                score = Integer.parseInt(num);
+                numberarr[index] = score * score * score;
+                next=true;
+                num= "";
+            }
+            if(i+1!=dartResult.length() && dartResult.charAt(i+1)=='*'){
+                numberarr[index] = numberarr[index] * 2;
+                if(index!=0){
+                    numberarr[index-1] = numberarr[index-1] * 2;
+                }
+            }
+            if(i+1!=dartResult.length() && dartResult.charAt(i+1)=='#'){
+                numberarr[index] = numberarr[index] * -1;
+            }
+            if(next){
+                next= false;
+                index++;
+            }
+        }
+        
+        for(int i=0;i<numberarr.length;i++){
+            answer += numberarr[i];
+        }
+        
+        return answer;
+    }
+}
