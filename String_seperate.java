cebc68dd54403ffbb0f5bb76b09c9488dc846fed
minwook <minukhan>
2024-09-27 15:49:38 +0900

    String_seperate

---
+++ String_seperate.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(String s) {
+        
+        char[] arr = s.toCharArray();
+        
+        int count=0;
+        int xcount =1;
+        int answer= 0;
+        char x = arr[0];
+        
+        if(arr.length==1){
+            answer++;
+        }
+        
+        for(int i=1;i<arr.length;i++){
+
+            if(x==arr[i]){
+                xcount++;
+            }
+            else{
+                count++;
+            }
+            
+            if(i == arr.length-1){
+                    answer++;
+                    break;
+            }
+            
+            if(xcount==count){
+                    x = arr[i+1];
+                    answer++;
+                    if(i+1==arr.length-1){
+                        answer++;
+                        break;
+                    }
+                    i++;
+                    xcount=1;
+                    count=0;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
