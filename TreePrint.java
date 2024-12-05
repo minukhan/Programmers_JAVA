97cfd2a44699cd2ec046f7b812d7022e85d7024c
minwook <minukhan>
2024-12-05 20:27:37 +0900

    TreePrint

---
+++ TreePrint.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class TreePrint {
+
+    public static void TreePrint(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("           ").append("*********").append("           ").append("\n");
+        sb.append("      ").append("*****").append("         ").append("****").append("       ").append("\n");
+        sb.append("    ").append("**").append("                  ").append("**").append("     ").append("\n");
+        sb.append("   ").append("*").append("                      ").append("*").append("    ").append("\n");
+        sb.append(" ").append("**").append("                        ").append("**").append("  ").append("\n");
+        sb.append("*").append("                            ").append("*").append(" ").append("\n");
+        sb.append("*").append("                            ").append("*").append(" ").append("\n");
+        sb.append("**").append("                          ").append("**").append(" ").append("\n");
+        sb.append(" ").append("**").append("  ").append("*").append("    ").append("****").append("  ").append("****").append("    ").append("*").append("  ").append("**").append("  ").append("\n");
+        sb.append("   ").append("**").append(" ").append("*****").append(" ").append("******").append(" ").append("*****").append(" ").append("**").append("    ").append("\n");
+        sb.append("             ").append("****").append("              ").append("\n");
+        sb.append("             ").append("****").append("              ").append("\n");
+        sb.append("             ").append("****").append("              ").append("\n");
+        sb.append("             ").append("****").append("              ").append("\n");
+        sb.append("            ").append("******").append("             ").append("\n");
+        sb.append("           ").append("********");
+        
+            
+        
+        System.out.print(sb);
+    }
+}
