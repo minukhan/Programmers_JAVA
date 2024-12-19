8b443b96e46c3a31613c206b270716e1b1a63d2d
minwook <minukhan>
2024-12-19 16:17:04 +0900

    Phone_List

--- Phone_List.java
+++ Phone_List.java
@@ -16,3 +16,42 @@ class Phone_List {
         return answer;
     }
 }
+
+//second Solution
+
+/*
+ * import java.util.Arrays;
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i=0;i<phone_book.length;i++){
+            set.add(phone_book[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        for(String value : set){
+            
+            char[] arr = value.toCharArray();
+            
+            for(int j=0;j<arr.length-1;j++){
+                sb.append(arr[j]);
+                
+                if(set.contains(sb.toString())){
+                    answer = false;
+                    break;
+                }
+                
+            }
+            sb.delete(0, sb.length());
+        }
+        
+        
+        return answer;
+    }
+}
+ */
